af9a9dd95d38a3adfc7acca35f28dab17433ad9c
Jeffrey Osazuwa <osa2@umbc.edu>
2021-05-19 16:42:21 -0400

    updated

--- ournewproject/ourapp/models.py
+++ ournewproject/ourapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=400)
+    address = models.CharField(max_length=400)
+    postal_code = models.IntegerField()
+
+class Student(models.Model):
+    School = models.ForeignKey(School, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    date_of_resumption = models.DateField(default=datetime.today)
