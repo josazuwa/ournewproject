0f2537ecf22dfd6714c28d60c7c048d09d24d8dd
Jeffrey Osazuwa <osa2@umbc.edu>
2021-05-19 20:51:19 -0400

    project complete

--- ournewproject/ourapp/admin.py
+++ ournewproject/ourapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import School, Student
+from django.db.models.expressions import F
+from .models import School, Student, Faculty, Department, Certificate_Type, Grade
 
 # Register your models here.
 admin.site.register(School)
 admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Department)
+admin.site.register(Certificate_Type)
+admin.site.register(Grade)
--- ournewproject/ourapp/models.py
+++ ournewproject/ourapp/models.py
@@ -2,15 +2,33 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-
 class School(models.Model):
-    name = models.CharField(max_length=400)
-    address = models.CharField(max_length=400)
-    postal_code = models.IntegerField()
-
+    name = models.CharField(max_length=400, default = "")
+    def __str__(self):
+        return self.name
+class Faculty(models.Model):
+    name = models.CharField(max_length=80)
+    def __str__(self):
+        return self.name
+class Department(models.Model):
+    name = models.CharField(max_length=80)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+class Certificate_Type(models.Model):
+    name = models.CharField(max_length=80)
+    def __str__(self):
+        return self.name
+class Grade(models.Model):
+    classYear = models.CharField(max_length=1, default = "")
+    def __str__(self):
+        return str(self.classYear)
 class Student(models.Model):
-    School = models.ForeignKey(School, on_delete=models.PROTECT)
-    first_name = models.CharField(max_length=40)
-    last_name = models.CharField(max_length=40)
-    age = models.IntegerField()
-    date_of_resumption = models.DateField(default=datetime.today)
+    fullname = models.CharField(max_length=80, default = "")
+    year_of_graduation = models.IntegerField()
+    department = models.ForeignKey(Department, default = "", on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, default = 0, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, null=True, on_delete=models.CASCADE, blank=True)
+    certificate_type = models.ForeignKey(Certificate_Type, default = "", on_delete=models.CASCADE)
+    def __str__(self):
+        return self.fullname
